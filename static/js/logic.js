e8ca00334cd3c7c3994401fbc2d1e376145cac99
joey-october85 <abelrod@live.com>
2023-07-20 22:35:03 -0500

    updated code with legend and css

--- static/js/logic.js
+++ static/js/logic.js
@@ -4,28 +4,26 @@
 let myMap = L.map("map", {
     center: [44.089, -103.82],
     zoom: 6
-  });
-  
- 
+});
 
-  // Adding a tile layer (the background map image) to our map:
-  // We use the addTo() method to add objects to our map.
-  L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-  }).addTo(myMap);
+
+
+
+// Adding a tile layer (the background map image) to our map:
+// We use the addTo() method to add objects to our map.
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(myMap);
 
 // API
-  let url = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson"
-  let promise = d3.json(url);
-  console.log(promise)
+let url = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson"
+let promise = d3.json(url);
+console.log(promise)
 
-  function markerSize(mgntd) {
-    
-  }
-promise.then(function(data) {
+promise.then(function (data) {
 
-        let info = data.features;
-        
+    let info = data.features;
+
     for (let i = 0; i < info.length; i++) {
         let location = info[i].geometry.coordinates;
         let magnitude = info[i].properties.mag;
@@ -42,40 +40,40 @@ promise.then(function(data) {
             magcolor = "#cf0606"
         } else if (depth > 69) {
             magcolor = "#b54e04"
-        } else if (depth > 49){
+        } else if (depth > 49) {
             magcolor = "#fc710d"
-        } else if (depth > 29){
+        } else if (depth > 29) {
             magcolor = "#fcb603"
-        } else if (depth > 9){
+        } else if (depth > 9) {
             magcolor = "#fcf403"
-        } else {magcolor = "#32a852"}
+        } else { magcolor = "#32a852" }
 
 
-        L.circle([location[1],location[0]], {
+        L.circle([location[1], location[0]], {
             fillOpacity: 0.45,
-            color:"black",
+            color: "black",
             weight: .5,
             fillColor: magcolor,
-            radius: magnitude*15000
+            radius: magnitude * 15000
         }).bindPopup(`<h4>${title} (Depth: ${depth}) <hr> ${date}</h4>`).addTo(myMap)
-
-        let leg = L.control({
-            position: "bottomright"
-        });
-
-        leg.onAdd = function() {
-            let div = L.DomUtil.create("div", "legend");
-            return div;
-        }
-
-        leg.addTo(myMap)
-
-
-
     }
 
+    // Set up the legend.
+    let legend = L.control({ position: "bottomright" });
+    legend.onAdd = function () {
+        let div = L.DomUtil.create("div", "info legend");
+        let colors = ["#32a852", "#fcf403", "#fcb603", "#fc710d", "#b54e04", "#cf0606"]
+        let labels = [-10, 10, 30, 50, 70, 90];
 
-    })
+        for (let i = 0; i < labels.length; i++) {
+            div.innerHTML += "<i style='background: " + colors[i] + "'></i> "
+                + labels[i] + (labels[i + 1] ? "&ndash;" + labels[i + 1] + "<br>" : "+");
+        }
+        return div;
+    };
 
-    
+    // Adding the legend to the map
+    legend.addTo(myMap);
 
+
+});
